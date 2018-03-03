27a49710eb9b6dce990057869f2b80ac217642fe
Bill Sheng <billxsheng@gmail.com>
2018-03-03 17:53:53 -0500

    Initial Commit

---
+++ Mind Diary/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Mind Diary
+//
+//  Created by Bill Sheng on 2018-03-03.
+//  Copyright © 2018 Bill Sheng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
