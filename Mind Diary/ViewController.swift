8d4e68c9c59f350b7ea55df53879c52899187646
Richard Xie <wolf@v1022-rn-243-145.campus-dynamic.uwaterloo.ca>
2018-03-04 17:38:04 -0500

    First Commit

--- Mind Diary/ViewController.swift
+++ Mind Diary/ViewController.swift
@@ -7,19 +7,19 @@
 //
 
 import UIKit
+import ToneAnalyzerV3
 
-class ViewController: UIViewController {
 
+
+class ViewController: UIViewController, UITextFieldDelegate {
+   
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
     }
-
-
 }
 
